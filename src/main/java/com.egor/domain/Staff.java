12c97d8134ce7d77093875f2fec305fcf465e6d7
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-06-10 16:17:19 +0400

    add Address, City, Country, FilmText, Inventory, Store entities

---
+++ src/main/java/com.egor/domain/Staff.java
@@ -0,0 +1,22 @@
+package com.egor.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(schema = "movie", name = "staff")
+public class Staff {
+    @Id
+    @Column(name = "inventory_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Byte id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+}
