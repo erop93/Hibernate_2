e31c5e00acf1118656d6624f5a8a1795e9e4bfcf
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-06-10 17:31:34 +0400

    add Customer, Payment, Rental and Staff entities

---
+++ src/main/java/com.egor/domain/Customer.java
@@ -0,0 +1,45 @@
+package com.egor.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(schema = "movie", name = "customer")
+public class Customer {
+    @Id
+    @Column(name = "customer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @Column(name = "active", columnDefinition = "BIT")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private Boolean isActive;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @Column(name = "last_update")
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+}
