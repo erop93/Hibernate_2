b38e559439a156d1ef0c969d372ba08b9d9ddd05
Egor Kondrashov <erop.kondrashov@yandex.ru>
2024-06-13 00:13:10 +0400

    add method for buying inventory with payment

--- src/main/java/com.egor/dao/FilmDAO.java
+++ src/main/java/com.egor/dao/FilmDAO.java
@@ -2,9 +2,17 @@ package com.egor.dao;
 
 import com.egor.domain.Film;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class FilmDAO extends GenericDAO<Film> {
     public FilmDAO(SessionFactory sessionFactory) {
         super(Film.class, sessionFactory);
+    }
+
+    public Film getFirstAvaliableFilmForRent() {
+        Query<Film> query = getCurrentSession().createQuery("select f FROM Film f " +
+                "where f.id not in (select distinct film.id from Inventory )", Film.class);
+        query.setMaxResults(1);
+        return query.getSingleResult();
     }
 }
